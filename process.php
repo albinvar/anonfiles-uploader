e4a510b99ca18325029500636342683a945fb6ce
Albin Varghese <albin8285@gmail.com>
2020-12-16 13:51:06 +0530

    upload and get json resoponse from server

--- process.php
+++ process.php
@@ -2,3 +2,11 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use App\Uploader;
+
+$token = "Your API Token";
+
+$uploader = new Uploader($token);
+$result = $uploader->decodeJson();
+
+var_dump($result);
--- src/Uploader.php
+++ src/Uploader.php
@@ -5,6 +5,9 @@ namespace App;
 class Uploader
 {
 	
+	protected $url;
+	protected $json;
+	
 	public function __construct($api)
 	{
 		$this->setApi($api);
@@ -16,5 +19,37 @@ class Uploader
 		$this->curlInit();
 	}
 	
+	protected function curlInit()
+	{
+		$this->ch = curl_init($this->url);
+	    curl_setopt($this->ch, CURLOPT_POST, 1);
+	    curl_setopt($this->ch, CURLOPT_POSTFIELDS, [
+        'file' => curl_file_create(
+            $_FILES['file']['tmp_name'],
+            $_FILES['file']['type'],
+            $_FILES['file']['name']
+        ),
+    ]);
+
+    curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    $this->createJson();
+	}
 	
+	protected function createJson()
+	{
+		$this->json = curl_exec($this->ch);
+		$this->closeCurl();
+		return $this->json;
+	}
+	
+	protected function closeCurl()
+	{
+		curl_close($this->ch);
+	}
+	
+	public function decodeJson()
+	{
+		$result = json_decode($this->createJson());
+		return $result;
+	}
 }
